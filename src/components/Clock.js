7667b0ae6ad7ae7e56169478f795f926f73ef9a2
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-01-27 09:40:09 +0100

    Before demo

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,41 +1,36 @@
 import React, { Component, useState, useEffect } from 'react';
 
-// class Clock extends Component {
-// handle = 0
-//   state = { now: new Date() };
+class Clock extends Component {
+  handle = 0;
+  state = { now: new Date() };
 
-//   componentDidMount() {
-//     this.handle = setInterval(() => this.setState({ now: new Date() }), 1000);
-//   }
+  componentDidMount() {
+    this.handle = setInterval(() => this.setState({ now: new Date() }), 1000);
+  }
 
-//   componentWillUnmount() {
-// if (this.handle){
-//     clearInterval(this.handle);
-//   }
-//   }
+  componentWillUnmount() {
+    if (this.handle) {
+      clearInterval(this.handle);
+    }
+  }
 
-//   render() {
-//     const { now } = this.state;
+  render() {
+    const { now } = this.state;
 
-//     return (
-//       <div className="center">The time is: {now.toLocaleTimeString()}</div>
-//     );
-//   }
-// }
-
-function useNow() {
-  const [now, setNow] = useState(new Date());
-
-  useEffect(() => {
-    const handle = setInterval(() => setNow(new Date()), 1000);
-    return () => clearInterval(handle);
-  }, []);
-  return now;
+    return (
+      <div className="center">The time is: {now.toLocaleTimeString()}</div>
+    );
+  }
 }
 
-const Clock = () => {
-  const now = useNow();
-  return <div className="center">The time is: {now.toLocaleTimeString()}</div>;
-};
+// const Clock = () => {
+//   const [now, setNow] = useState(new Date());
 
-export default Clock;
+//   useEffect(() => {
+//     const handle = setInterval(() => setNow(new Date()), 1000);
+//     return () => clearInterval(handle);
+//   }, []);
+//   return <div className="center">The time is: {now.toLocaleTimeString()}</div>;
+// };
+
+// export default Clock;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,50 +1,37 @@
-import React, { Component, useState, useReducer } from 'react';
+import React, { Component, useState } from 'react';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'increment':
-      return { ...state, counter: state.counter + 1 };
-    default:
-      return state;
+export class Counter extends Component {
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="center">
+        <div>Count: {count}</div>
+        <div>
+          <button onClick={this.increment}>Increment</button>
+        </div>
+      </div>
+    );
   }
 }
 
-const Counter = () => {
-    const [count, setCount] = useState(0);
-//   const [state, dispatch] = useReducer(reducer, { counter: 0 });
+// const Counter = () => {
+//     const [count, setCount] = useState(0);
 
-  return (
-    <div className="center">
-      <div>Count: {count}</div>
-      <div>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        {/* <button onClick={() => dispatch({ type: 'increment' })}>
-          Increment
-        </button> */}
-      </div>
-    </div>
-  );
-};
-
-// export class Counter extends Component {
-//   state = { count: 0 };
-
-//   increment = () => {
-//     this.setState(state => ({ count: state.count + 1 }));
-//   };
-
-//   render() {
-//     const { count } = this.state;
-
-//     return (
-//       <div className="center">
-//         <div>Count: {count}</div>
-//         <div>
-//           <button onClick={this.increment}>Increment</button>
-//         </div>
+//   return (
+//     <div className="center">
+//       <div>Count: {count}</div>
+//       <div>
+//         <button onClick={() => setCount(count + 1)}>Increment</button>
 //       </div>
-//     );
-//   }
-// }
+//     </div>
+//   );
+// };
 
 export default Counter;
